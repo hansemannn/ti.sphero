60dedc9dabb8225aa8fbbdf1e6a677421412da79
Hans Knoechel <hknoechel@appcelerator.com>
2016-03-17 19:38:46 +0100

    Ingore build

--- ios/build/ti.sphero.build/Release-iphoneos/ti.sphero.build/Objects-normal/arm64/TiSpheroModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hans/Documents/appcelerator_modules/ti.sphero/ios/Classes/TiSpheroModuleAssets.m \
-  /Users/hans/Documents/appcelerator_modules/ti.sphero/ios/Classes/TiSpheroModuleAssets.h
